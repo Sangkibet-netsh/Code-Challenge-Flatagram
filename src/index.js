25e57f196e99e261c801d6a076e3d5d665a2c5fe
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-04-01 08:32:06 +0300

    Initial  commit declared variables to hold json links

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 // write your code here
+// Fetches the image data from the server and displays it on the page
+//declare variables for json file links
+const imageUrl = "http://localhost:3000/images";
+const commentsUrl = "http://localhost:3000/comments";
+
+//Fetch Dog Details
+function fetchDogDetails (id) {
+    fetch(`${imageUrl}/${id}`)
+    .then(response => response.json())
+    .then((dog) => {
+        document.getElementById("card-title").innerText = dog.title
+
+        document.getElementById("card-image").src = dog.image;
+    })}
